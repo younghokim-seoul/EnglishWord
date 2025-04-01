553566db450720452cc4c19a5bb8d74cdf19a285
cmyh91 <cmyh91@golfzon.com>
2025-04-01 09:58:35 +0900

    1.초기화면 tabview 추가 , 2.상세 deps 초기화면 추가

---
+++ lib/feature/pages/home/widget/human_tab_view.dart
@@ -0,0 +1,52 @@
+import 'package:bounce_tapper/bounce_tapper.dart';
+import 'package:englishword/core/style/app_color.dart';
+import 'package:englishword/core/style/app_text_style.dart';
+import 'package:englishword/feature/pages/home/provider/topics_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class HumanTabView extends HookConsumerWidget {
+  const HumanTabView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    useAutomaticKeepAlive();
+
+    final topics = ref.watch(sortedTopicsProvider);
+    return GridView.builder(
+      padding: const EdgeInsets.symmetric(vertical: 40,horizontal: 20),
+      shrinkWrap: true,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12,
+        mainAxisSpacing: 11,
+        childAspectRatio: 162 / 53
+      ),
+      itemCount: topics.length,
+      itemBuilder: (context, index) {
+        final topic = topics[index];
+
+        return BounceTapper(
+          onTap: () {},
+          highlightColor: AppColor.of.yellow2,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              border: Border.all(color: AppColor.of.background1),
+            ),
+            child: Center(
+              child: Text(
+                topic,
+                style: AppTextStyle.headline3,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/feature/pages/step/first_step_page.dart
@@ -0,0 +1,20 @@
+import 'package:englishword/feature/base/base_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/src/consumer.dart';
+
+class FirstStepPage extends BasePage {
+  const FirstStepPage({super.key});
+
+  @override
+  Widget buildPage(BuildContext context, WidgetRef ref) {
+    throw UnimplementedError();
+  }
+
+  @override
+  PreferredSizeWidget? buildAppBar(BuildContext context, WidgetRef ref) {
+    // TODO: implement buildAppBar
+    return super.buildAppBar(context, ref);
+  }
+
+}
